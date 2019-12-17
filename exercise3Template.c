b4cc2d339efca9a1eb3793e56d92ccd28cfe4ef9
Luster_Flame <kurasiki.homura@gmail.com>
2019-12-17 18:44:35 +0800

    Changed file layout & filenames. Deleted unfit .h files.

---
+++ exercise3Template.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* myStrdup(const char* original){
+	char *newStr;
+
+	/*
+		implement here
+	*/
+
+	return newStr;
+}
+
+int main(){
+	// Declair string and duplicate
+	char* original = "The ORIGINAL string";
+	char* new = myStrdup(original);
+
+	// Exit if duplication failed.
+	if(new == NULL){
+		printf("Memory allocation failed.\n");
+		exit(-1);
+	}
+
+	// Print the strings
+	printf("Original String: \"%s\"\n", original);
+	printf("Copied   String: \"%s\"\n", new);
+
+	// Change "the" int "not" in the duplicate string
+	new[0] = 'N';
+	new[1] = 'o';
+	new[2] = 't';
+	printf("Copied   String: \"%s\"\n", new);
+	
+	return 0;
+}
--- myStrdupEx.c
+++ myStrdupEx.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "myStrdup.h"
+#include <string.h>
+
+char* myStrdup(const char* original){
+	// Create space for the new string to return.
+	// +1 is for the terminate charcter ('\0').
+	char *newStr = malloc(strlen(original) + 1);
+
+	// Check if malloc() succeed
+	if(!newStr){
+		return NULL;
+	}
+
+	// Copy the thing in s into newStr
+	strcpy(newStr, original);
+
+	return newStr;
+}
 
 int main(){
 	// Declair string and duplicate
